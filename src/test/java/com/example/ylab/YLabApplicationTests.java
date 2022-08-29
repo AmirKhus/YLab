e489a81b6531ffda78749103186be5a3148caeac
amir1 <amir19040268@gmail.com>
2022-08-30 00:52:36 +0300

    First commit

--- src/test/java/com/example/ylab/YLabApplicationTests.java
+++ src/test/java/com/example/ylab/YLabApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class YLabApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
