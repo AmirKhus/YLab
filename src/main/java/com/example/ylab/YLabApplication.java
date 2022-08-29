a8559b27c1794fcb839e693bd1144220b6fdcf8f
amir1 <amir19040268@gmail.com>
2022-08-30 00:48:20 +0300

    First commit

---
+++ src/main/java/com/example/ylab/YLabApplication.java
@@ -0,0 +1,13 @@
+package com.example.ylab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YLabApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YLabApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/ylab/YLabApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ylab;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YLabApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
