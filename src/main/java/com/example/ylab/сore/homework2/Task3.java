f88266ac6249c3642d17dc4db07f2660bd97e65c
AmirKhus <70978948+AmirKhus@users.noreply.github.com>
2022-09-09 03:30:07 +0300

    Update Task3.java

--- src/main/java/com/example/ylab/сore/homework2/Task3.java
+++ src/main/java/com/example/ylab/сore/homework2/Task3.java
@@ -2,17 +2,38 @@ package com.example.ylab.сore.homework2;
 
 public class Task3 {
     public static void main(String[] args) {
-        System.out.println(fuzzySearch("cwheeel", "cartwheel"));
+        System.out.println(fuzzySearch("car", "ca6$$#_rtwheel")); // true
+        System.out.println(fuzzySearch("cwhl", "cartwheel")); // true
+        System.out.println(fuzzySearch("cwhee", "cartwheel")); // true
+        System.out.println(fuzzySearch("cartwheel", "cartwheel")); // true
+        System.out.println(fuzzySearch("cwheeel", "cartwheel")); // false
+        System.out.println(fuzzySearch("lw", "cartwheel")); // false
     }
-    private static boolean fuzzySearch(String world, String text) {
-        int kursor = 0;
-        for (int i = 0; i < text.length(); i++) {
-            if (text.charAt(i) == world.charAt(kursor)) {
-                if (world.length()-1 == kursor)
-                    return true;
-                kursor++;
-            }
+    private static boolean fuzzySearch(String searchWord, String string) {
+        int indexString = 0;
+        int fromIndexString = 0;
+
+        if(searchWord == null || string == null)
+            return false;
+
+
+        if (searchWord.length() > string.length())
+            return false;
+
+
+        if (searchWord.length() == string.length())
+            return string.contains(searchWord);
+
+
+        while (indexString < searchWord.length()) {
+            fromIndexString = string.indexOf(searchWord.charAt(indexString), fromIndexString);
+            indexString++;
+            fromIndexString++;
+
+            if (fromIndexString == 0)
+                return false;
         }
-        return false;
+        return true;
+
     }
 }
